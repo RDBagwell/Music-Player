70210a5d04c0ece51366959197709adf79eeb02c
Robert Bagwell <erbagwell@yahoo.com>
2020-08-10 20:48:34 -0600

    add play pause function

--- js/script.js
+++ js/script.js
@@ -8,12 +8,14 @@ let isPlaying = false;
 function playMusic(){
     isPlaying = true;
     playBtn.classList.replace('fa-play','fa-pause');
+    playBtn.setAttribute('title', 'pause');
     music.play();
 }
 
 function pauseMusic(){
     isPlaying = false;
     playBtn.classList.replace('fa-pause','fa-play');
+    playBtn.setAttribute('title', 'play');
     music.pause();
 }
 
